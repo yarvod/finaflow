62ae6cd946a364962666551ce086a822673fafd4
yarvod <yar.vod.ole@gmail.com>
2023-04-15 02:24:59 +0300

    modify useradmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,9 +1,56 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin, GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import (
+    UserAdmin as BaseUserAdmin,
+    GroupAdmin as BaseGroupAdmin,
+)
 from django.contrib.auth.models import Group
 
 from .models import User, CustomGroup
 
 admin.site.unregister(Group)
-admin.site.register(User, UserAdmin)
 admin.site.register(CustomGroup, BaseGroupAdmin)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Персональная информация",
+            {"fields": ("first_name", "last_name", "username")},
+        ),
+        (
+            "Права",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Даты активности", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
